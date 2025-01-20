a407fa97008a83ac42e5e61c6d4d6e148bf5b82b
ChengTze-Wu <yaoop3050777@gmail.com>
2025-01-21 00:17:27 +0800

    refactor(PlatformLayout): remove PlatformNavbar and integrate UserButton for user authentication

--- app/platform/layout.tsx
+++ app/platform/layout.tsx
@@ -1,6 +1,7 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
 import Sidebar from "@/components/platform/sidebar";
 import PlatformBreadcrumbs from "@/components/platform/breadcrumbs";
-import PlatformNavbar from "@/components/platform/navbar";
 
 export default function PlatformLayout({
   children,
@@ -10,10 +11,12 @@ export default function PlatformLayout({
   return (
     <div className="flex">
       <Sidebar className="fixed inset-y-0 left-0 shadow-sm" />
-      <div className="flex-1 p-4 flex flex-col min-h-screen ml-64 overflow-hidden">
-        <PlatformNavbar />
+      <div className="flex-1 px-4 flex flex-col min-h-screen ml-64 overflow-hidden">
         <nav className="flex items-center justify-between my-4">
           <PlatformBreadcrumbs />
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </nav>
         <main className="flex-1">{children}</main>
       </div>
