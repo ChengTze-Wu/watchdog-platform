01d549c0ea95da49af544b77526b4b04a2dbfec5
ChengTze-Wu <yaoop3050777@gmail.com>
2025-01-27 19:47:53 +0800

    feat(Platform): integrate OfflineTable for displaying disconnected devices

--- app/platform/page.tsx
+++ app/platform/page.tsx
@@ -1,9 +1,19 @@
 import { getConnectedDevices } from "@/actions/connected-devices";
+import { getDisconnectedDevices } from "@/actions/disconnected-devices";
 
-import OnlineTable from "@/components/platform/online/online-table";
+import OnlineTable from "@/components/platform/devices/online-table";
+import OfflineTable from "@/components/platform/devices/offline-table";
 
 export default async function Platform() {
-  const connectedDevice = await getConnectedDevices();
+  const [connectedDevice, disconnectedDevice] = await Promise.all([
+    getConnectedDevices(),
+    getDisconnectedDevices(),
+  ]);
 
-  return <OnlineTable connectedDevice={connectedDevice.data} />;
+  return (
+    <>
+      <OnlineTable connectedDevice={connectedDevice.data} />
+      <OfflineTable disconnectedDevice={disconnectedDevice.data} />
+    </>
+  );
 }
