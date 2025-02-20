e82a81810138d665f6e428c4d8aad81a9b881c02
ChengTze-Wu <yaoop3050777@gmail.com>
2025-02-20 22:33:56 +0800

    refactor(loading): remove unused Loading component

--- app/platform/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@heroui/react";
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-full">
-      <Spinner color="default" size="lg" />
-    </div>
-  );
-}
