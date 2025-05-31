1a87cca99518faa601a29d49e9310201bf2d8ef6
ChengTze-Wu <yaoop3050777@gmail.com>
2025-05-31 12:07:03 +0800

    fix: adjust padding in layout component for better spacing

--- app/platform/layout.tsx
+++ app/platform/layout.tsx
@@ -12,7 +12,7 @@ export default function PlatformLayout({
   return (
     <>
       <Sidebar className="shadow-sm fixed top-0 left-0 h-full w-64 hidden lg:grid" />
-      <div className="px-4 flex flex-col flex-1 overflow-hidden lg:ml-64">
+      <div className="px-2 lg:px-4 flex flex-col flex-1 overflow-hidden lg:ml-64">
         <nav className="flex items-center justify-between py-6">
           <div className="flex items-center gap-1">
             <Menu className="lg:hidden" />
